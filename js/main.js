65a2d5f5decd1ece0c4ad20111c08ecda3e6cbaf
Jamie Thompson <jthompson@geo-jobe.com>
2017-11-07 21:30:39 -0600

    added some functions to buttons in different ways

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 window.onload = function(){
+    document.getElementById("mailForm").addEventListener("submit", formSubmit);
+
     var accord = document.getElementsByClassName("accordion"),
         closeBtn = document.getElementsByClassName("close-btn"),
         closeBoxContainer = document.getElementsByClassName("close-box-container");
@@ -11,9 +13,27 @@ window.onload = function(){
             panel.style.display = "none";
         else
             panel.style.display = "block";
-    },
+    };
 
     closeBtn[0].onclick = function(){
         closeBoxContainer[0].style.display = "none";
+    };
+
+    function formSubmit(e){
+        e.preventDefault();
+        console.log("E:", e);
+        var alertText = "";
+        for(var i = 0; i < e.srcElement.length - 1; i++){
+            alertText += e.srcElement[i].name + ": ";
+
+            if(e.srcElement[i].value !== ""){
+                alertText += e.srcElement[i].value;
+            } else{
+                alertText += "N/A";
+            }
+
+            alertText += "\n";
+        }
+        alert(alertText);
     }
 };
